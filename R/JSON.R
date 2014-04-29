a56bd104b5f92366d03ea2496293792dcda84bbb
Markus List <mlist@health.sdu.dk>
2014-04-29 14:52:17 +0200

    another UTF-8 issue fix

--- R/JSON.R
+++ R/JSON.R
@@ -218,7 +218,7 @@ rppa.load <- function (connection=NULL, barcode=NA, slideIndex=NA, securityToken
   
   spots <- rppa.set.blocksPerRow(spots, as.integer(scan(text=getURL(blocksUrl, curl=connection), what = "integer")))
   attr(spots, "PMT") <- as.integer(scan(text=getURL(pmtUrl, curl=connection), what = "integer"))
-  spots <- rppa.set.title(spots, paste(scan(text=getURL(titleUrl, curl=connection), what = "character"), collapse=" "))
+  spots <- rppa.set.title(spots, paste(scan(text=getURL(titleUrl, curl=connection), what = "character", fileEncoding="UTF-8"), collapse=" "))
   spots <- rppa.set.antibody(spots, paste(scan(text=getURL(antibodyUrl, curl=connection), what = "character"), collapse=" "))
   attr(spots, "slideIndex") <- slideIndex
   message("...everything done. returning data.")
